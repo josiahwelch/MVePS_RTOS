f0b70320aee20e260426361137913e85a0785959
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-08-08 14:36:10 -0500

    fixed stack organization

--- MVePSRtLoop.h
+++ MVePSRtLoop.h
@@ -1,4 +1,4 @@
-// 
+//
 #include <iostream>
 #include <map>
 #include <bits/stdc++.h>
@@ -13,30 +13,33 @@ namespace std {
 	class MVePSRtLoop {
 		private:
 			map<int, function<void()>> m_functions;
+			int m_current = 0; // Pointer to the last index written in m_functions/m_stack
 			map<int, pair<unsigned long, int>> m_stack; // A bit convoluted, but basically <function id, <interval, priority>>
 			unsigned long m_initial_millisecond;
 			bool running = false;
 
-			static void m_organizeStack(map<unsigned long, map<int, vector<int>>> *organized_stack, map<int, pair<unsigned long, int>> unorganized_stack) {
-				int funct_id;
-				unsigned long interval;
-				int priority;
+			static void m_organizeStack(map<unsigned long, map<int, vector<int>>> &organized_stack, const map<int, pair<unsigned long, int>> &unorganized_stack) {
 				for (auto const& element : unorganized_stack) {
-					funct_id = element.first;
-					interval = element.second.first;
-					priority = element.second.second;
-					if (organized_stack->count(interval) != 0) {
+					int funct_id = element.first;
+					unsigned long interval = element.second.first;
+					int priority = element.second.second;
+					if (organized_stack.count(interval) != 0) {
 						if (organized_stack[interval].count(priority) != 0) {
-							
+							organized_stack[interval][priority].push_back(funct_id);
+						} else {
+							organized_stack[interval][priority] = vector<int>({funct_id});
 						}
+					} else {
+						organized_stack[interval] = map<int, vector<int>>({priority, {funct_id}});
 					}
-					organized_stack[interval] = make_pair(funct_id, priority);
 				}
 			}
 
 		public:
 			void addToStack(function<void()> func, int priority, unsigned long interval) {
-				m_stack[func] = pair<unsigned long, int>(interval, priority);
+				m_functions[m_current] = func;
+				m_stack[m_current] = {interval, priority};
+				++m_current;
 			}
 
 			void start() {
@@ -44,15 +47,10 @@ namespace std {
 			}
 
 			void run() {
-				map<unsigned long, map<int, vector<function>>> organized_m_stack;
+				map<unsigned long, map<int, vector<int>>> organized_m_stack;
 				if(running) {
-
+					m_organizeStack(organized_m_stack, m_stack);
 					for(auto const& element : organized_m_stack) {
-							std::cout << element.first  // string (key)
-							  << ':'
-							  << element.second // string's value
-							  << std::endl;
-
 							if(millis() % element.first == 0) {
 								for(auto const& element : element.second) {
 
